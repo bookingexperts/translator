486c24131421bcb1b1cbb29eafde3896472adc6e
behrooz shabani (everplays) <everplays@gmail.com>
2016-02-10 17:03:32 +0100

    implemented direct submission to gengo and fetch from it

--- lib/tasks/translator_tasks.rake
+++ lib/tasks/translator_tasks.rake
@@ -1,42 +1,60 @@
 namespace :translator do
-  desc "Export missing translations for a specific locale"
-  task :export_keys => :environment do
-    from = ENV['FROM']
-    to   = ENV['TO']
-    if from.present? and to.present?
-      translator = Translator::Translator.new(
-        from: from.to_sym,
-        to:   to.to_sym
-      )
-      missing = translator.export_keys
-      if missing.length > 0
-        filename = "translate_#{from}_to_#{to}.txt"
-        File.open(filename, 'w') do |file|
-          file.write missing
-        end
-        puts "Created export file: #{filename}"
-      else
-        puts "There are no missing translations from #{from} to #{to}"
+
+  desc 'Export missing translations for a specific locale'
+  task export_keys: :environment do
+    next unless check_params 'FROM', 'TO'
+    missing = translator.export_keys
+    if missing.length > 0
+      filename = "translate_#{from}_to_#{to}.txt"
+      File.open(filename, 'w') do |file_handler|
+        file_handler.write missing
       end
+      puts "Created export file: #{filename}"
     else
-      puts "Please provide locale to translate for example:"
-      puts "rake translator FROM=en TO=fr"
+      puts "There are no missing translations from #{from} to #{to}"
     end
   end
 
-  desc "Import missing translations for a specific locale"
-  task :import_keys => :environment do
-    if ENV['FROM'].present? and ENV['TO'].present? and ENV['FILE'].present?
-      translator = Translator::Translator.new(
-        from: ENV['FROM'].to_sym,
-        to:   ENV['TO'].to_sym
-      )
-      file = File.open(ENV['FILE'])
-      translator.import_keys(file.read)
-      translator.write_locale_file
-    else
-      puts "Please provide the following arguments:"
-      puts "rake translator FROM=en TO=fr FILE=en_to_fr.translate"
+  desc 'Import missing translations for a specific locale'
+  task import_keys: :environment do
+    next unless check_params 'FROM', 'TO', 'FILE'
+    file_handler = File.open file
+    translator.import_keys file_handler.read
+    translator.write_locale_file
+  end
+
+  desc 'submits the translations to gengo'
+  task submit_to_gengo: :environment do
+    next unless check_params 'FROM', 'TO'
+    translator.submit_to_gengo
+  end
+
+  desc 'fetches the translations from gengo'
+  task fetch_from_gengo: :environment do
+    Translator::Translator.read_orders.each do |order|
+      translator(order).fetch_from_gengo order[:id]
     end
   end
+
+  %w(FROM TO FILE).each do |param|
+
+    define_method param.downcase do
+      ENV[param]
+    end
+
+  end
+
+  def check_params *params
+    all_are_present = params.all? { |param| ENV[param].present? }
+    unless all_are_present
+      STDERR.puts "usage example: rake translator FROM=en TO=fr FILE=en_to_fr.translate"
+    end
+    all_are_present
+  end
+
+  def translator **params
+    Translator::Translator.new from: (params[:from] || from).to_sym,
+      to: (params[:to] || to).to_sym
+  end
+
 end
--- lib/translator.rb
+++ lib/translator.rb
@@ -1,24 +1,82 @@
 require 'yaml'
+require 'gengo'
 
 module Translator
+
   class Engine < Rails::Engine
   end
 
   class Translator
+
+    attr_reader :from, :to, :directory, :comments
+
     def initialize(options = {})
       @from = options[:from]
-      @to   = options[:to]
-      @dir  = options[:dir] || "config/locales/multilingual"
+      @to = options[:to]
+      @comments = options[:comments]
+      @directory = options[:directory] || "config/locales/multilingual"
+      @orders = self.class.read_orders
     end
 
     def prepare_translations_for_missing_keys
-      I18n.with_locale(@from) do
+      I18n.with_locale(from) do
         result = {}
         find_missing_keys.each do |key|
-          result[key] = I18n.t(key.gsub("#{@to}.", '')).to_s
+          result[key] = I18n.t(key.gsub("#{to}.", '')).to_s
         end
         result
       end
+    end
+
+    def submit_to_gengo
+      jobs = gengo_jobs
+      if jobs.empty?
+        puts "Nothing to translate from #{from} to #{to}"
+        return
+      end
+      response = gengo.postTranslationJobs jobs: jobs
+      @orders << { id: response['response']['order_id'].to_i, to: to, from: from }
+      write_orders
+    end
+
+    def fetch_from_gengo order_id
+      order = gengo.getTranslationOrderJobs order_id: order_id
+      job_ids = []
+      %w(
+        jobs_available jobs_pending jobs_reviewable jobs_approved jobs_revising
+      ).each do |list|
+        job_ids.concat order['response']['order'][list]
+      end
+      jobs = gengo.getTranslationJobs(ids: job_ids)['response']['jobs']
+      @import = {}
+      jobs.each do |job|
+        next if job['body_tgt'].blank?
+        @import[job['custom_data']] = job['body_tgt']
+      end
+      finalize_order order_id if @import.count == jobs.count
+    end
+
+    def gengo_jobs
+      position_index = 0
+      pairs = prepare_translations_for_missing_keys.map do |key, content|
+        [
+          key,
+          {
+            body_src: content,
+            force: 0,
+            comment: comments,
+            lc_src: from,
+            lc_tgt: to,
+            tier: 'standard',
+            custom_data: key,
+            type: 'text',
+            as_group: 1,
+            position: (position_index += 1),
+            slug: [Rails.application.class.parent_name, key].join(': ')
+          }
+        ]
+      end
+      Hash[pairs]
     end
 
     def export_keys
@@ -42,30 +100,48 @@ module Translator
     end
 
     def write_locale_file
-      old_yaml    = yaml(@to)
+      old_yaml    = yaml(to)
       new_yaml    = deflatten_keys(@import)
       merged_yaml = old_yaml ? old_yaml.deep_merge(new_yaml) : new_yaml
-      File.open(path(@to), 'w') do |file|
+      File.open(path(to), 'w') do |file|
         file.write merged_yaml.to_yaml
       end
     end
 
     def find_missing_keys
-      yaml_1 = yaml(@from)
-      yaml_2 = yaml(@to)
+      yaml_1 = yaml(from)
+      yaml_2 = yaml(to)
       keys_1 = yaml_1.present? ? flatten_keys(yaml_1[yaml_1.keys.first]) : []
       keys_2 = yaml_2.present? ? flatten_keys(yaml_2[yaml_2.keys.first]) : []
-      (keys_1 - keys_2).map {|k| "#{@to}.#{k}" }
+      (keys_1 - keys_2).map {|k| "#{to}.#{k}" }
+    end
+
+    def self.read_orders
+      orders = []
+      if File.exists? translation_file
+        File.open translation_file do |file_handler|
+          orders = JSON.parse file_handler.read
+        end
+      end
+      orders.map(&:symbolize_keys)
     end
 
   private
+
+    def finalize_order order_id
+      @orders.reject! do |order|
+        order[:id] == order_id
+      end
+      write_orders
+      write_locale_file
+    end
 
     def yaml(locale)
       YAML.load((File.open(path(locale)) rescue ''))
     end
 
     def path(locale)
-      File.expand_path("#{@dir}/#{locale}.yml")
+      File.expand_path("#{directory}/#{locale}.yml")
     end
 
     def deflatten_keys(hash)
@@ -90,5 +166,32 @@ module Translator
       end
       prefix == "" ? keys.flatten : keys
     end
+
+    def gengo
+      Gengo::API.new api_version: '2',
+        sandbox: false,
+        public_key: Rails.application.secrets.gengo_public_key,
+        private_key: Rails.application.secrets.gengo_private_key
+    end
+
+    def translation_file
+      self.class.translation_file
+    end
+
+    def write_orders
+      if @orders.empty?
+        File.unlink translation_file if File.exists? translation_file
+      else
+        File.open translation_file, 'w+' do |file_handler|
+          file_handler.write @orders.to_json
+        end
+      end
+    end
+
+    def self.translation_file
+      Rails.root.join '.in_progress_translations'
+    end
+
   end
+
 end
